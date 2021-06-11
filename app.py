0fae227bf57d5b52ee800f726758375a843b6577
Susan Adesoji <adesojisusan.adeola@gmail.com>
2021-06-11 20:11:26 +0100

    eval function

--- app.py
+++ app.py
@@ -4,22 +4,73 @@ import tkinter
 root = tkinter.Tk()
 root.title('Calculator')
 
-def add():
+expression = ""
+
+def add(value):
+    global expression
+    expression += value
+    print(expression)
+    
+def clear():
     pass
 
-root_label= tkinter.Label(root, text='42')
-root_label.grid(row=0, column=0)
+def calculate():
+    global expression
+    result = eval(expression)
+    root_label.config(text= result)
 
-button_1 = tkinter.Button(root, text='1', command=add())
+root_label= tkinter.Label(root, text='')
+root_label.grid(row=0, column=0 , columnspan=4)
+
+button_1 = tkinter.Button(root, text='1', command=lambda:add('1'))
 button_1.grid(row= 1, column = 0)
 
-button_2 = tkinter.Button(root, text ='2', command = add())
+button_2 = tkinter.Button(root, text ='2', command =lambda: add('2'))
 button_2.grid(row=1, column = 1)
 
-button_3 = tkinter.Button(root, text ='3', command = add())
+button_3 = tkinter.Button(root, text ='3', command = lambda:add('3'))
 button_3.grid(row=1, column = 2)
 
+button_divide = tkinter.Button(root, text ='/', command = lambda: add('/'))
+button_divide.grid(row=1, column = 3)
 
+button_4 = tkinter.Button(root, text='4', command=lambda:add('4'))
+button_4.grid(row= 2, column =0)
 
+button_5 = tkinter.Button(root, text ='5', command =lambda: add('5'))
+button_5.grid(row=2, column = 1)
+
+button_6 = tkinter.Button(root, text ='6', command = lambda:add('6'))
+button_6.grid(row=2, column = 2)
+
+button_muiltiply = tkinter.Button(root, text ='*', command = lambda: add('*'))
+button_muiltiply.grid(row=2, column = 3)
+
+button_7 = tkinter.Button(root, text='7', command=lambda:add('7'))
+button_7.grid(row= 3, column =0)
+
+button_8 = tkinter.Button(root, text ='8', command =lambda: add('8'))
+button_8.grid(row=3, column = 1)
+
+button_9 = tkinter.Button(root, text ='9', command = lambda:add('9'))
+button_9.grid(row=3, column = 2)
+
+button_addition = tkinter.Button(root, text ='+', command = lambda: add('+'))
+button_addition.grid(row=3, column = 3)
+
+button_Clear = tkinter.Button(root, text='C', command=lambda: clear())
+button_Clear.grid(row= 4, column =0)
+
+button_0 = tkinter.Button(root, text ='0', command =lambda: add('0'))
+button_0.grid(row=4, column = 1)
+
+button_dot = tkinter.Button(root, text ='.', command = lambda:add('.'))
+button_dot.grid(row=4, column = 2)
+
+button_substract = tkinter.Button(root, text ='-', command = lambda: add('-'))
+button_substract.grid(row=4, column = 3)
+
+button_equal= tkinter.Button(root, text='=', command= lambda:calculate())
+button_equal.grid(row=5 , column= 0 , columnspan= 4)
 
 root.mainloop()
